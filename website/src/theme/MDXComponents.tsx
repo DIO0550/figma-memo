7d547e5c2e4d3e74d7a57e32d61e1716b29c812d
DIO0550 <diot12345@yahoo.co.jp>
2025-06-28 12:20:35 +0000

    ✨ [New Feature]: DocumentImageコンポーネントを追加
    - 画像表示用の再利用可能なコンポーネントを作成
    - 統一されたスタイリング（ボーダー、角丸、シャドウ）を適用
    - MDXComponentsに登録してMDXファイルで利用可能に
    - React.FCを使わない現代的な書き方で実装

--- website/src/theme/MDXComponents.tsx
+++ website/src/theme/MDXComponents.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import MDXComponents from '@theme-original/MDXComponents';
 import Card from '@site/src/components/Card';
+import DocumentImage from '@site/src/components/DocumentImage';
 
 export default {
   // Re-use the default mapping
   ...MDXComponents,
   // Map the "<Card>" tag to our Card component
   Card,
+  // Map the "<DocumentImage>" tag to our DocumentImage component
+  DocumentImage,
 };
