7e318db5d2b7d593cd8b1a26238c55c7e60728ab
DIO0550 <diot12345@yahoo.co.jp>
2025-07-13 11:21:49 +0000

    ✨ [New Feature]: CardGridコンポーネントをMDXで自動利用可能に設定

--- website/src/theme/MDXComponents.tsx
+++ website/src/theme/MDXComponents.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import MDXComponents from '@theme-original/MDXComponents';
 import Card from '@site/src/components/Card';
+import CardGrid from '@site/src/components/Card/CardGrid';
 import DocumentImage from '@site/src/components/DocumentImage';
 
 export default {
@@ -8,6 +9,8 @@ export default {
   ...MDXComponents,
   // Map the "<Card>" tag to our Card component
   Card,
+  // Map the "<CardGrid>" tag to our CardGrid component
+  CardGrid,
   // Map the "<DocumentImage>" tag to our DocumentImage component
   DocumentImage,
 };
